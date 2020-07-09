2e5da9d1dee9d572932fc48ddc7b6d696ab73bb8
adept-bell-4492 <garlandegoma@gmail.com>
2020-07-09 23:22:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,10 +5,15 @@ def nyc_pigeon_organizer(data)
   binding.pry
 value.each do |inner_key,names|
   names.each do |n|
-    if !final_array[name]
-      final_array[name]={}
+    if !final_array[n]
+      final_array[n]={}
     end
-    if !final_array[name][key]
-      !
+    if !final_array[n][key]
+      !final_array[n][key]=[]
+    end
+    results[n][key].push(inner_key.to_s)
+  end
+  #binding.pryfin
+end
 end
 end
